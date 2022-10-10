c16f88e5998538c3946287989e3011764543e302
YoonMinSoo <minsoo9615@naver.com>
2022-10-10 17:27:28 +0900

    create delete posts

--- instapjt/posts/views.py
+++ instapjt/posts/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
-
+import re
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods, require_POST, require_safe
+from .models import Post
+from .forms import PostForm
 # Create your views here.
+
+@require_safe
 def index(request):
-    return render(request, "posts/index.html")
+    posts = Post.objects.order_by("created_at")
+    context = {
+        "posts" : posts,
+    }
+    return render(request, "posts/index.html", context)
+
+@require_http_methods(["GET", "POST"])
+def create(request):
+    if request.method=="POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("posts:index")
+    else:
+        form = PostForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, "posts/create.html", context)
+
+@require_POST
+def delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect("posts:index")
